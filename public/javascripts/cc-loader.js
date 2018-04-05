2a0471c9d34bd0500dd88bb31e075ccf61c42bad
Yke ten Donkelaar <yketd@hotmail.com>
2018-04-05 12:18:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- public/javascripts/cc-loader.js
+++ public/javascripts/cc-loader.js
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 import {$, jQuery} from 'jquery';
 
 var bg = $(".content");
 var colorbutton = $(".colorchanger");
 export { bg, colorbutton };
+=======
+export var bg = $(".content");
+export var colorbutton = $(".colorchanger");
+>>>>>>> Stashed changes
